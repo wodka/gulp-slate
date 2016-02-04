e06908fe15e652f05bc09af07e05e06d82355d1c
wodka <michael.schramm@gmail.com>
2016-02-04 12:18:18 +0100

    move assets creation into slate() call

--- index.js
+++ index.js
@@ -2,21 +2,105 @@
 
 var _ = require('lodash');
 var fs = require('fs');
-var gutil = require('gulp-util');
+var es = require('event-stream');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 var through = require('through2');
 var slate = require('./src/slate');
 var Promise = require('promise');
 
+/**
+ * set the current file in given path to name
+ *
+ * @param path
+ * @param name
+ *
+ * @returns {string}
+ */
+function changeFile (path, name) {
+    var parts = path.split('/');
+    parts.pop();
+    parts.push(name);
+    return parts.join('/');
+}
+
+/**
+ * change the name to the override if provided else just remove .md file ending
+ *
+ * @param name
+ * @param override
+ *
+ * @returns {string}
+ */
+function fixFilename (name, override) {
+    if (override) {
+        return changeFile(name, override);
+    }
+
+    return changeFile(name, name.split('/').pop().replace(/\.md/, ""));
+}
+
+/**
+ * build assets
+ *
+ * @param opts
+ *
+ * @returns Promise
+ */
+function buildAssets (opts) {
+    return new Promise(function (resolve) {
+        es.concat(
+            gulp
+                .src('src/app.scss')
+                .pipe(sass())
+                .pipe(gulp.dest(opts.target + 'build/')),
+            gulp
+                .src('node_modules/slate/source/javascripts/app/*.js')
+                .pipe(concat("app.js"))
+                .pipe(uglify())
+                .pipe(gulp.dest(opts.target + 'build/')),
+            gulp
+                .src(
+                    [
+                        'node_modules/slate/source/javascripts/lib/_jquery.js',
+                        'node_modules/slate/source/javascripts/lib/_jquery_ui.js',
+                        'node_modules/slate/source/javascripts/lib/*.js'
+                    ]
+                )
+                .pipe(concat("libs.js"))
+                .pipe(uglify())
+                .pipe(gulp.dest(opts.target + 'build/')),
+            gulp
+                .src(
+                    [
+                        'node_modules/slate/source/fonts/*',
+                        'node_modules/slate/source/images/navbar.png'
+                    ]
+                )
+                .pipe(gulp.dest(opts.target + 'build/')),
+            gulp
+                .src(opts.logo)
+                .pipe(gulp.dest(opts.target + 'images/'))
+        ).on(
+            'end',
+            function () {
+                resolve('');
+            }
+        );
+    })
+}
+
 module.exports = function (opts) {
     opts = _.extend(opts, {
-        path: 'dist/',
+        target: 'dist/',
+        targetWrite: true,
         filename: false,
         template: 'src/layout.html',
-        analytics: {
-            google: false
-        },
+        logo: 'node_modules/slate/source/images/logo.png',
         includeLoader: function (name, mainFile) {
-            return new Promise(function (resolve, reject) {
+            return new Promise(function (resolve) {
                 fs.readFile(
                     changeFile(mainFile, "includes/_"+name+".md"),
                     'utf-8',
@@ -33,42 +117,46 @@ module.exports = function (opts) {
         }
     });
 
-    function changeFile (path, name) {
-        var parts = path.split('/');
-        parts.pop();
-        parts.push(name);
-        return parts.join('/');
-    }
-
-    function fixFilename (name) {
-        if (opts.filename) {
-            return changeFile(name, opts.filename);
-        }
-
-        return changeFile(name, name.split('/').pop().replace(/\.md/, ""));
+    if (opts.targetWrite) {
+        buildAssets(opts);
     }
 
     return through.obj(
         function (file, enc, cb) {
              // file coming in :D
+            var context = this;
 
-            fs.readFile(opts.template, 'utf8', function (err, source) {
-                slate(
-                    file.contents.toString(),
-                    source,
-                    function (name) {
-                        // call internal file loader
-                        return opts.includeLoader(name, file.path);
-                    }
-                ).then(
-                    function(result) {
-                        file.contents = new Buffer(result);
-                        file.path = fixFilename(file.path);
+            var files = [];
+            if (opts.targetWrite) {
+                files.push(buildAssets(opts));
+            }
 
-                        cb(null, file);//null, fileCreator(opts.filename, result, { src: true }));
-                    }
-                );
-            });
+            files.push(new Promise(function (resolve) {
+                fs.readFile(opts.template, 'utf8', function (err, source) {
+                    slate(
+                        file.contents.toString(),
+                        source,
+                        function (name) {
+                            // call internal file loader
+                            return opts.includeLoader(name, file.path);
+                        }
+                    ).then(
+                        function(result) {
+                            file.contents = new Buffer(result);
+                            file.path = fixFilename(file.path, opts.filename);
+
+                            context.push(file);
+                        }
+                    );
+                });
+            }));
+
+            Promise
+                .all(files)
+                .then(function () {
+                    console.log('finished building everything');
+                    cb();
+                });
         }
     );
 };
--- src/slate.js
+++ src/slate.js
@@ -1,14 +1,12 @@
-/*
- Copyright 2015 Jesse Manek
- Licensed under the Apache License, Version 2.0 (the "License"); you may
- not use this file except in compliance with the License. You may obtain
- a copy of the License at
- http://www.apache.org/licenses/LICENSE-2.0
- Unless required by applicable law or agreed to in writing, software
- distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- License for the specific language governing permissions and limitations
- under
+/**
+ * logic to parse markdown files
+ *
+ * @see https://github.com/jmanek/slate_node
+ *
+ * @author Michael Schramm (changes to integrate with gulp)
+ * @author Jesse Manek (creator of https://github.com/jmanek/slate_node)
+ *
+ * @private
  */
 
 var _ = require('lodash');
@@ -103,7 +101,7 @@ module.exports = function (markup, template, includesLoader) {
 
     data['languages'] = JSON.stringify(data['language_tabs']);
 
-    return new Promise(function (resolve, reject) {
+    return new Promise(function (resolve) {
         Promise.all(markdown)
             .then(
                 function (res) {
